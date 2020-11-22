697e703e9ae3c6e1a483a67debbe7153f9095d0e
Vaibhav Srivastava <vaibhavsrivastava8991@gmail.com>
2020-11-22 14:28:55 +0530

    Adding sign-up feature

--- src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './sign-in-and-sign-up.style.scss';
-
+ 
 import SignIn from './../sign-in/sign-in.component';
+import SignUp from '../sign-up/sign-up.component';
+
 
 const SignInAndSignUp = () => (
     <div className = 'sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 
 );   
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,25 +1,107 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
+ 
 const config = {
-    apiKey: "AIzaSyBxfPnCTzTbNEsZTKKcQ6QnM8emiMuI-Kc",
-    authDomain: "crwn-db-764ef.firebaseapp.com",
-    databaseURL: "https://crwn-db-764ef.firebaseio.com",
-    projectId: "crwn-db-764ef",
-    storageBucket: "crwn-db-764ef.appspot.com",
-    messagingSenderId: "593931824225",
-    appId: "1:593931824225:web:55336558d93822f2c60e20",
-    measurementId: "G-4C2HTJ96RK"
-  };
+  apiKey: "AIzaSyBxfPnCTzTbNEsZTKKcQ6QnM8emiMuI-Kc",
+  authDomain: "crwn-db-764ef.firebaseapp.com",
+  databaseURL: "https://crwn-db-764ef.firebaseio.com",
+  projectId: "crwn-db-764ef",
+  storageBucket: "crwn-db-764ef.appspot.com",
+  messagingSenderId: "593931824225",
+  appId: "1:593931824225:web:55336558d93822f2c60e20",
+  measurementId: "G-4C2HTJ96RK"
+  }; 
+
+  export const createUserProfileDocument = async (userAuth , additionalData) => {
+    if(!userAuth) return;
+    const userRef =  firestore.doc(`users/${userAuth.uid}`);  
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,  
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user');
+      }
+    }
+  
+    return userRef;
+
+  }
+
 
   firebase.initializeApp(config);
 
+
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
+
 
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({prompt: 'select_account'});
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
   export default firebase;
+  
+
+  /*
+
+  import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = { 
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
+
+  */
